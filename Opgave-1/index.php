63b4fa15ab1f4d08e563cfb14d22e52f02c3af16
pk1260 <18251@ma-web.nl>
2019-10-03 11:35:10 +0200

    Change-2

--- Opgave-1/index.php
+++ Opgave-1/index.php
@@ -18,18 +18,16 @@
 </head>
 <body>
 <div class="wrapper">
-    <form action="">
-        <input type="text"      name="Voornaam"     value="voornaam"        minlength="">
-        <input type="text"      name="Achternaam"   value="achternaam"      minlength="">
-        <input type="text"      name="Straat"       value="straat"          minlength="">
-        <input type="number"    name="Huisnr"       value="huisnr"          minlength="">
-        <input type="number"    name="Postcode"     value="postcode-nummer" minlength="">
-        <input type="text"      name=""             value="postcode-letter" minlength="">
-        <input type="text"      name="Woonplaats"   value="woonplaats"      minlength="">
-        <input type="email"     name="Email"        value="email"           minlength="">
-        <input type="tel"       name="Telefoon"     value="telefoonnummer"  minlength="">
+    <form action="form.php" class="form">
+        <input type="text"      name="Voornaam"     value="voornaam"        maxlength="25"  class="inputfile"                       >   <br>
+        <input type="text"      name="Achternaam"   value="achternaam"      maxlength="128" class="inputfile"                       >   <br>
+        <input type="text"      name="Straat"       value="straat"          maxlength="48"  class="inputfile"       id="straat"     >
+        <input type="text"      name="Huisnr"       value="huisnr"          maxlength="11"  class="inputfile"       id="nummer"     >   <br>
+        <input type="text"      name="Postcode"     value="postcode-nummer" maxlength="6"   class="inputfile"                       >   <br>
+        <input type="text"      name="Woonplaats"   value="woonplaats"      maxlength="24"  class="inputfile"                       >   <br>
+        <input type="email"     name="Email"        value="email"           maxlength="254" class="inputfile"                       >   <br>
+        <input type="tel"       name="Telefoon"     value="telefoonnummer"  maxlength="11"  class="inputfile"                       >   <br>
         <input type="submit"                        value="Verzend">
     </form>
 </div>
 </body>
-</html>
