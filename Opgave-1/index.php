8c41064650fa0f3b4e227627dffaef1f5b6d6e87
pk1260 <18251@ma-web.nl>
2019-10-03 11:07:08 +0200

    Change-1

---
+++ Opgave-1/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pascalkuiper
+ * Date: 2019-10-03
+ * Time: 10:46
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="main.css">
+    <title>CSS Template</title>
+</head>
+<body>
+<div class="wrapper">
+    <form action="">
+        <input type="text"      name="Voornaam"     value="voornaam"        minlength="">
+        <input type="text"      name="Achternaam"   value="achternaam"      minlength="">
+        <input type="text"      name="Straat"       value="straat"          minlength="">
+        <input type="number"    name="Huisnr"       value="huisnr"          minlength="">
+        <input type="number"    name="Postcode"     value="postcode-nummer" minlength="">
+        <input type="text"      name=""             value="postcode-letter" minlength="">
+        <input type="text"      name="Woonplaats"   value="woonplaats"      minlength="">
+        <input type="email"     name="Email"        value="email"           minlength="">
+        <input type="tel"       name="Telefoon"     value="telefoonnummer"  minlength="">
+        <input type="submit"                        value="Verzend">
+    </form>
+</div>
+</body>
+</html>
